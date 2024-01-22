011fdac0e2e0e5743ff15a3c4d8640821837bcb7
anzelinux <155091432+anzelinux@users.noreply.github.com>
2024-01-22 23:13:46 +0100

    Add files via upload

--- sommaNNumeriDoWh.cpp
+++ sommaNNumeriDoWh.cpp
@@ -10,14 +10,12 @@ int n,conta;
 conta=0;
 
 //calcolo quanti numeri interi sono stati inseriti con ciclo while
-
-while (n!=0) {
+do {
 cout<<"inserisci un numero o premi il tasto 0 per interrompere il ciclo";
 cin>>n;
 conta=conta+1;
 
-}
-
+} while (n!=0); 
 cout<<"sono stati inseriti "<<conta<<" numeri, incluso il numero 0, utilizzato per interrompere il ciclo";
 
 system("pause");
